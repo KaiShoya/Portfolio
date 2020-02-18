f71b2c39d6597b4a931c087d35bfc6d1645bf2d8
Kai Shoya <s-kai@lumber-mill.co.jp>
2020-02-18 17:24:43 +0900

    Initial commit.

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-spacer />
+      <v-btn icon href="https://twitter.com/shonari123" target="_blank">
+        <v-icon>mdi-twitter</v-icon>
+      </v-btn>
+      <v-btn icon href="https://github.com/KaiShoya" target="_blank">
+        <v-icon>mdi-github-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+        <!-- <v-card-actions>
+          <v-spacer />
+          <v-btn color="primary" nuxt to="/inspire">Continue</v-btn>
+        </v-card-actions> -->
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {}
+</script>
